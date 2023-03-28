04c4fee19c66903193739267107516503352e51f
CDayL <dayanaparck@gmail.com>
2023-03-28 00:50:08 -0600

    Recarga de la bala y control de numero de balas video 14

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,8 @@ public class Player : MonoBehaviour
     [SerializeField] Camera camera; // Referencia a la camera
     Vector2 facingDirection; // Direccion a la que voltea la mira o jugador
     [SerializeField] Transform bulletPrefab;// Referencia a la bala o prefab
+    bool gunLoaded = true; //Saber si esta cargada con una bala
+    [SerializeField] float fireRate = 1; // Variable para controlar numero de balas
 
     // Start is called before the first frame update
     void Start()
@@ -42,14 +44,28 @@ public class Player : MonoBehaviour
 
         //CREACION DE BALA
 
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && gunLoaded)
         {
+            // No podre disparar ya que no esta cargada el arma
+            gunLoaded = false;
             // Obtener angulo entre jugador y mira para darlo en radianes
             float angle = Mathf.Atan2(facingDirection.y, facingDirection.x) * Mathf.Rad2Deg;
             // Crear un Quaternion para sacar la rotacion que queremos
             Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
             //Crear bala en la posicion del jugador con su rotacion
             Instantiate(bulletPrefab, transform.position, targetRotation);
+            // Espera un segundo y recarga la bala permitiendote disparar
+            StartCoroutine(ReloadGun());
         }
+    }
+
+    // CORRUTINA (Creamos tiempo para recargar la bala)
+
+    IEnumerator ReloadGun ()
+    {
+        // Crear tiempo de retraso
+        yield return new WaitForSeconds(1/fireRate);
+        // Recargar el arma con una bala
+        gunLoaded = true;
     }
 }
