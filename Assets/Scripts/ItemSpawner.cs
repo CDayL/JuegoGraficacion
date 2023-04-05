5647a17cc6a97e11b6cf3a595d24010061d1ebd2
CDayL <dayanaparck@gmail.com>
2023-04-04 20:20:47 -0600

    Anadimos nuevas variables a el spawn de la pastilla de vida para mejor precisión, avance de video 21

--- Assets/Scripts/ItemSpawner.cs
+++ Assets/Scripts/ItemSpawner.cs
@@ -6,18 +6,20 @@ public class ItemSpawner : MonoBehaviour
 {
     //VARIABLES
     [SerializeField] GameObject checkpointPrefab; // Referencia al checkpoint
+    [SerializeField] int checkpointSpawnDelay = 10; // Cada 10 seg aparece una nueva pastilla de tiempo
+    [SerializeField] float spawnRadius = 10; // Radio presiso
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Llamado de la cortina
+        StartCoroutine(SpawnCheckpointRoutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Llamado de la cortina
-        StartCoroutine(SpawnCheckpointRoutine());
+
     }
     
     // CREAR PASTILLAS DE TIEMPO
@@ -25,10 +27,10 @@ public class ItemSpawner : MonoBehaviour
     {
         while (true)
         {
-            // Cada tres segundo aparece una nueva pastilla de tiempo
-            yield return new WaitForSeconds(3);
+            // Aparece una nueva pastilla de tiempo
+            yield return new WaitForSeconds(checkpointSpawnDelay);
             // Crear posicion aleatoria
-            Vector2 randomPosition = Random.insideUnitCircle * 5;
+            Vector2 randomPosition = Random.insideUnitCircle * spawnRadius;
             // Crear una pastilla, pasar la posicion
             Instantiate(checkpointPrefab, randomPosition, Quaternion.identity);
         }
